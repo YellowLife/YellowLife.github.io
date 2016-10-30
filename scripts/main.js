2c976ed5954e67b0dcd97c834004163168ff190f
haozhi1798 <haozhi1798@gmail.com>
2016-10-30 20:10:34 +0100

    home page game section completed

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	var home_navbar_button=$(".home-navbar-button");
+	var home_navbar_text=$(".home-nav-text");
+	var navbar_nav=$(".navbar-nav");
+
+	$(document).on({	
+		click: function(event){
+			if(!home_navbar_button.is(event.target)&&!home_navbar_text.is(event.target)){
+				navbar_nav.removeClass("home-navbar-show");
+			}
+    	}
+	});
+
+
+	home_navbar_button.on({
+		click: function(event){
+			if(!home_navbar_text.is(event.target)){
+				navbar_nav.toggleClass("home-navbar-show");
+			}
+		}
+	});
+
+
+	home_navbar_text.on({
+		click: function(event){
+			if(!home_navbar_button.is(event.target)){
+				navbar_nav.toggleClass("home-navbar-show");
+			}
+		}
+	});
+
+
+
+
+
+	
+
+});
+
